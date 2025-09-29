fd12f1ab31e15ddd2434c82814973d33daa2e689
RobinVorsselmans <robin.vorsselmans@cursist.syntra-mvl.be>
2025-09-29 20:10:28 +0200

    V.0.1 added a remark to def convert_to_seconds

--- exch1.py
+++ exch1.py
@@ -7,7 +7,7 @@ def main():
     print(convert_to_seconds(years, days, hours, minutes, seconds))
 
 def convert_to_seconds(y, d, h, m, s):
-    time_in_seconds = s + m * 60 + h * 3600 + d * 24 * 3600 + y * 365 * 24 * 3600
+    time_in_seconds = s + m * 60 + h * 3600 + d * 24 * 3600 + y * 365 * 24 * 3600 #this is a function that calculates time in seconds
     return time_in_seconds
 
 if __name__ == '__main__':
