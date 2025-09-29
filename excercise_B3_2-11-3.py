dcc7655e5943a4fc37f182939deb2e676b7922b3
Brent-Hendricx <brenthendricx1@gmail.com>
2025-09-29 21:41:11 +0200

    Oef B3_2-11-3 "deels gemaakt"

---
+++ excercise_B3_2-11-3.py
@@ -0,0 +1,15 @@
+#excercise_B3_2-11-3
+#P1
+import math
+
+radius = 5
+volume = 4/3 * math.pi * radius ** 3
+print(volume)
+
+#P2
+x = 42
+y = math.cos(x) ** 2 + math.sin(x) ** 2
+print(y)
+
+a = 1000
+print(math.log10(a) + 1) #het aantal cijfers in een getal berekenen
