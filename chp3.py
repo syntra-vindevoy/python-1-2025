eea1f166ee6efe19523de48a733abf248bdb8917
Brent-Hendricx <brenthendricx1@gmail.com>
2025-09-29 21:51:19 +0200

    chp3 eerste eigen versie

--- chp3.py
+++ chp3.py
@@ -3,7 +3,7 @@ char = '#'
 text = "brent"
 
 def line_with_stars(text):
-    padding = (len(text) + 1) // 2
+    padding = (max_char - len(text)) // 2
     return f"{char * padding} {text} {char * padding}"
 
 print(line_with_stars(text))
