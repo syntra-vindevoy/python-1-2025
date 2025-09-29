74151bcc2a2785e36f1633473a9c2f54aa6d51cc
Brent-Hendricx <brenthendricx1@gmail.com>
2025-09-29 21:40:26 +0200

    / en * verandert naar - en + voor float variabelen te vermijden.

--- switch_variable_values.py
+++ switch_variable_values.py
@@ -10,14 +10,33 @@ a = c
 #assert
 assert a == 3
 assert b == 2
-
 a = 2
 b = 3
 
 #CODE HERE (vers 2)
-a = a * b #6
-b = a / b #2
-a = a / b #3
+a = a + b #6
+b = a - b #2
+a = a - b #3
+
+#assert
+assert a == 3
+assert b == 2
+a = 2
+b = 3
+
+#CODE HERE (vers 3)
+a = a ^ b
+b = a ^ b
+a = a ^ b
+
+#assert
+assert a == 3
+assert b == 2
+a = 2
+b = 3
+
+#CODE HERE (vers 4)
+a, b = b, a
 
 #assert
 assert a == 3
