487a23ea1b496953cf52f3f4d46ec22d15f11240
Yves Vindevogel <yves@vindevogel.net>
2025-09-29 19:28:47 +0200

    First lessons

---
+++ debug.py
@@ -0,0 +1,14 @@
+def main():
+    a = 2
+    b = 5
+
+    assert a + b == 7
+    assert som(a, b) == 7
+
+def som(a, b):
+    c = a + b
+
+    return c
+
+if __name__ == "__main__":
+    main()
---
+++ hello.py
@@ -0,0 +1,64 @@
+import sys
+
+import debug
+
+sys.exit(0)
+
+print("hello", "world", sep="-", end="")
+
+me = "Yves Vindevogel"
+you = 'Brecht'
+
+
+print(type(me))
+
+a = 1
+b = 2
+c = a + b
+d = 2.5
+e = c + d
+print(e)
+print(type(e))
+
+print(2 + 2.0)
+
+print(1 - 2)
+print(2 * 7)
+print(2 / 3)
+print(-4 // 3)
+print(7 % 2)
+
+print(7 ^ 2)
+print(6 ^ 3)
+print(6 >> 2)
+print(6 >> 1)
+
+a = 1
+a = "Yves"
+b = "Toto"
+
+print(a + b)
+print("ud" * 30)
+
+
+punten = 90
+totaal = 100
+result = punten / totaal * 100
+print("U behaalde " + str(result) + " punten")
+print("U behaalde", result, "punten")
+print(f"U behaalde {result} punten")
+
+s = """
+sqfs
+fdsqf
+dsqfdsq
+fds
+q
+"""
+
+#
+# i = input("Geef een getal:")
+# print("U behaalde" + str(int(i)  / 100) + "percent")
+
+print(len("Yves"))
+
