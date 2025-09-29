c18a26cd039745481dfc04d098038a29cf59f753
Brent-Hendricx <brenthendricx1@gmail.com>
2025-09-29 21:05:43 +0200

    Extra oef 2 variabels hun waarden verwisselen op 2 manieren.

---
+++ switch_variable_values.py
@@ -0,0 +1,24 @@
+#oefeningen Extra (Wissel a en b van waarden)
+a = 2
+b = 3
+
+#CODE HERE (vers 1)
+c = b
+b = a
+a = c
+
+#assert
+assert a == 3
+assert b == 2
+
+a = 2
+b = 3
+
+#CODE HERE (vers 2)
+a = a * b #6
+b = a / b #2
+a = a / b #3
+
+#assert
+assert a == 3
+assert b == 2
