258bbfb4d160caf57c7177814ad98dba70c0ef06
RobinVorsselmans <robin.vorsselmans@cursist.syntra-mvl.be>
2025-09-29 19:57:11 +0200

    V.0 of first exercise of chapter 1

---
+++ exch1.py
@@ -0,0 +1,14 @@
+def main():
+    seconds = 42
+    minutes = 42
+    hours = 0
+    days = 0
+    years = 0
+    print(convert_to_seconds(years, days, hours, minutes, seconds))
+
+def convert_to_seconds(y, d, h, m, s):
+    time_in_seconds = s + m * 60 + h * 3600 + d * 24 * 3600 + y * 365 * 24 * 3600
+    return time_in_seconds
+
+if __name__ == '__main__':
+    main()
