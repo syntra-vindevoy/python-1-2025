ee11aaa6edab5567eeb1b790c9e7af32171aaeb3
Brent-Hendricx <brenthendricx1@gmail.com>
2025-10-01 18:18:29 +0200

    chp3 oplossing Yves staat er nu bij

--- chp3.py
+++ chp3.py
@@ -1,9 +1,21 @@
 max_char = 40
 char = '#'
-text = "brent"
 
+#Versie Brent
 def line_with_stars(text):
     padding = (max_char - len(text)) // 2
-    return f"{char * padding} {text} {char * padding}"
+    return f"{char * (padding - 1)} {text} {char * padding}"
 
-print(line_with_stars(text))
+#Versie Yves
+def starred_name(last_name, first_name):
+    name = last_name + ' ' + first_name
+    postfix = (max_char - 2 - len (name)) // 2
+    prefix = (max_char - 2 - len (name)) - postfix
+    print("*" * prefix, name, "*" * postfix)
+
+print(line_with_stars("Yves"))
+print(line_with_stars("Brent"))
+print(line_with_stars(""))
+print(char * max_char + "\n")
+starred_name("Vindevogel", "Yves")
+starred_name("Hendricx", "Brent")
