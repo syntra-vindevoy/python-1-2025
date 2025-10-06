f9b50402aadc1105314c32273ba5026690a34065
Brent-Hendricx <brenthendricx1@gmail.com>
2025-10-06 20:05:16 +0200

    Oef 3.11.5 klaar en werkend

--- ch3_excercise_11.py
+++ ch3_excercise_11.py
@@ -15,9 +15,22 @@ def rectangle(brick:str = "=", width:int = 3, height:int = 3):
         print(brick * width)
 
 # ----------------------------------------------------------------------------------------------------------------------------------
+# 3.11.5. Exercise
+# The song “99 Bottles of Beer” automated by verse:
+def bottle_verse(beers:int):
+    if beers < 0: return    # Safety net
+
+    for i in range(beers, 0, -1):
+        print(f"{i} bottles of beer on the wall\n")
+        if i == 0: return
+        print(f"{i} bottles of beer\n"
+              f"Take one down, pass it around\n")
+
+# ----------------------------------------------------------------------------------------------------------------------------------
 def main():
     triangle('L', 5)
     rectangle('H', 5, 4)
+    bottle_verse(10)
 
 if __name__ == "__main__":
     main()
