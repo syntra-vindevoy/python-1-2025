c616661ec4d48bce2796f7b8dbd9a728adb603d9
Brent-Hendricx <brenthendricx1@gmail.com>
2025-10-06 18:46:39 +0200

    python template v0.0.1

---
+++ template.py
@@ -0,0 +1,8 @@
+def say_hello(who: str):
+    print(f"Hello, {who}!")
+
+def main():                 # Standaard in elke code te zetten om "shadow names" te vermijden  (Gebruik met [if __name__ == "__main__":]    main())
+    name = "Brent"
+    say_hello(name)
+if __name__ == "__main__":  # Standaard in elke code te zetten om "shadow names" te vermijden (Gebruik met [def main():])
+    main()
