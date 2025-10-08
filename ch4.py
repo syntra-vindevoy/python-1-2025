48b18cc55e8d4890d41b3476e56db4a1b6f9c096
Brent-Hendricx <brenthendricx1@gmail.com>
2025-10-08 21:32:01 +0200

    Nu met voorbeelden van documentatie.

--- ch4.py
+++ ch4.py
@@ -19,12 +19,25 @@ def circle(size):
     polygon(size=size, sides=360)
 
 def polygon(size:int, sides:int):
+    # Dit is een manier van documentatie. Gebruik liever "numpy" voor een vorm van documentatie, zo krijg je die uitleg hier in pycharm als je over de functie staat met de muis.
+    """
+    This function creates a polygon with a given size and sides using a turtle.
+    :param size: the length of the sides
+    :param sides: the amount of sides
+    :return: none
+    :author: Brent Hendricx
+    :date: 2025-10-08
+    """
     bob = turtle.Turtle()
 
+  # Het volgende is een voorbeeld van documenteren. Eerste regel is de persoon die het schreef en datum. Tweede regel en voort is de uitleg. Die uitleg is wat voor de hand liggend.
+    # Brent Hendricx - 2025-10-08
+    # A circle is 360 degrees, for calculating the angle you take the 360° and divide by the amount of corners.
     for i in range(sides):
         bob.forward(size)
         bob.right(360/sides)
-
+  #Code die niet werkt, verwijder je niet weg en je zet er bij wat er niet werkte, waarom het niet werkte, en waarom het nu zo is.
+    #Dit werkt. De while wait(1) werkte ook, maar dit wordt gebruikt enkel voor turtles en dus duidelijker.
     turtle.mainloop()
 
 square = partial(polygon, sides=4)
