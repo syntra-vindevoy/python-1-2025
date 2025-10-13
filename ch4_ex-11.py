4c5d31895dcf55ebee57328f81fdbf83b6205c59
Brent-Hendricx <brenthendricx1@gmail.com>
2025-10-13 21:53:03 +0200

    Lijn 27 "test" is bij de oproeping vd functie vast berekend. Zoek berekening en steek het in "test". Verfijn.

--- ch4_ex-11.py
+++ ch4_ex-11.py
@@ -1,3 +1,4 @@
+import math
 from functools import partial
 import turtle
 
@@ -20,12 +21,49 @@ def parallelogram(side_horz:int, side_vert:int, angle:int):
         turtle.forward(side_vert)
         turtle.left(180-angle)
 
-    turtle.mainloop()
+def triangle_test(angle_point:float, height:int, test:float):
+    turtle.forward(height)
+    turtle.left(180-(180-angle_point)/2)
+    #test = math.sqrt(math.pow(height, 2)+math.pow(a, 2))*2
+    turtle.forward(math.sqrt(math.pow(height, 2) - math.pow(test, 2))*2)
+    #turtle.forward(height²-test²=c²)
+    turtle.left(180-(180-angle_point)/2)
+    turtle.forward(height)
+    turtle.left(180)
+
+#def triangle_sss(height:int, side:int):
+#def triangle_sas(height:int, side:int, angle:int):
+def triangle_asa(height:int, angle_point:float, angle_twin:float):
+    turtle.forward(height)
+    turtle.left(angle_twin)
+    turtle.forward(height)
+    turtle.left(angle_twin)
+    turtle.forward(height)
+    turtle.left(180)
+    """
+    turtle.forward(height)
+    #turtle.left(180-angle_twin*2)
+    turtle.left(angle_twin)
+    turtle.forward(height)
+    #turtle.left(180-angle_twin*2)
+    turtle.left(angle_twin)
+    turtle.forward(height)
+    turtle.left(180)
+    """
+
+def draw_pie(height:int, triangle_amount:int):
+    for i in range(triangle_amount):
+        triangle_asa(height=height, angle_point=360/triangle_amount, angle_twin=180-(360/triangle_amount))
 
 def main():
     make_turtle()
     #rectangle(side_horz=80, side_vert=40)
-    rhombus(side_horz=50, side_vert=50, angle=60)
+    #rhombus(side_horz=50, side_vert=50, angle=60)
+    #triangle_asa(height=80, angle_point=30, angle_twin=80)
+    #draw_pie(height=80, draw_pie)
+    triangle_test(angle_point=40, height=200, test=187.94)
+
+    turtle.mainloop()
 
 if __name__ == "__main__":
     main()
