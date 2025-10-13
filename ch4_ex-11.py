a2ee022a94619fd398735b285216a11c94fbbd55
Brent-Hendricx <brenthendricx1@gmail.com>
2025-10-13 17:04:50 +0200

    Ex3 klaar (versie functie in functie)

--- ch4_ex-11.py
+++ ch4_ex-11.py
@@ -6,23 +6,11 @@ from jupyturtle import make_turtle
 
 # Exercise 1
 def rectangle(side_horz:int, side_vert:int):
-    for i in range(2):
-        turtle.forward(side_horz)
-        turtle.left(90)
-        turtle.forward(side_vert)
-        turtle.left(90)
-
-    turtle.mainloop()
+    parallelogram(side_horz=side_horz, side_vert=side_vert, angle= 90)
 
 # Exercise 2
-def rhombus(side:int, angle:int):
-    for i in range(2):
-        turtle.forward(side)
-        turtle.left(angle)
-        turtle.forward(side)
-        turtle.left(180-angle)
-
-    turtle.mainloop()
+def rhombus(side_horz:int, side_vert:int, angle:int):
+    parallelogram(side_horz=side_horz, side_vert=side_vert, angle= angle)
 
 # Exercise 3
 def parallelogram(side_horz:int, side_vert:int, angle:int):
@@ -33,15 +21,11 @@ def parallelogram(side_horz:int, side_vert:int, angle:int):
         turtle.left(180-angle)
 
     turtle.mainloop()
-rectangle_parl = partial(parallelogram,angle=90)
-rhombus_parl = partial(parallelogram)
 
 def main():
     make_turtle()
     #rectangle(side_horz=80, side_vert=40)
-    #rhombus(side=50, angle=60)
-    #rectangle_parl(side_horz=80, side_vert=40)
-    rhombus_parl(side_horz=80, side_vert=40, angle=60)
+    rhombus(side_horz=50, side_vert=50, angle=60)
 
 if __name__ == "__main__":
     main()
