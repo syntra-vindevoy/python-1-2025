8d4dd3b709e83cfe132ab06e6fb6028d5477eb85
Brent-Hendricx <brenthendricx1@gmail.com>
2025-10-22 17:11:27 +0200

    Inspanning (lopend) om programma uit te leggen en in stappen te delen.

--- trees_per_sqr_meter.py
+++ trees_per_sqr_meter.py
@@ -1,5 +1,7 @@
 def first_tree_plus_rest(opp_height:int, opp_width:int, tree_sqr_radius:int):
-
+    # Bereken de kleinste driehoek die past, pak de breedte, en trek het af van de maximum breedte.
+    
+    # Deel de nieuwe breedte door de boom diameter
     #breedte - afstand eerste boom
     opp_height -= tree_sqr_radius
     return 0
@@ -14,6 +16,8 @@ def main():
     while opp_height > tree_sqr_radius & opp_height > tree_sqr_radius:
         aantal_bomen += first_tree_plus_rest(opp_height, opp_width, tree_sqr_radius)
         aantal_bomen += opp_width // tree_sqr_radius
+
+    print(aantal_bomen)
     #HIER PROGRAMMEREN
 
 if __name__ == "__main__":
