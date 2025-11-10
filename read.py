7d929ef99c39ad2d5aff50a1e6fc90e103dcce3f
Brent Hendricx <brenthendricx1@gmail.com>
2025-11-10 21:25:48 +0100

    Small update to reference the ability to write described in tp3_ch8

--- read.py
+++ read.py
@@ -6,6 +6,8 @@
 # file.close() # Altijd sluiten zo snel mogelijk als je het niet meer nodig hebt.
 # Doe DIT.
 # Methode 1
+from urllib3.filepost import writer
+
 with open("words.txt", "r") as file:
     lines = file.readlines()
 
@@ -20,3 +22,12 @@ with open("words.txt", "r") as file:
 
         if not line:
             break
+
+#EXTRA from tp3_ch8 (read, write, and read/write as binary
+#Writing
+# with open("words.txt", "w") as file:
+#     writer.write(line)
+#     writer.close()
+
+# with open("words.txt", "rb") as file:     #rb = Read as Binary
+# with open("words.txt", "wb") as file:     #wb = Write as Binary
