bafd0fd35be6a9f60aadf3759ea1f07dc10f2957
Brent Hendricx <brenthendricx1@gmail.com>
2025-11-10 21:27:15 +0100

    Name change to add prefix "tp3_" describing programs built using "Think Python version 3"

--- tp3_ch7_9_ex.py
+++ tp3_ch7_9_ex.py
@@ -37,7 +37,6 @@ def uses_all(word, required):
             return False
     return True
 
-
 def check_word(word, available, required):
     """Check whether a word is acceptable.
 
@@ -52,8 +51,10 @@ def check_word(word, available, required):
     >>> check_word('bee', 'ACDLORT', 'R')
     False
     """
-    assert required.lower() in available.lower()
-    if len(word) < 4 or required.lower() not in word.lower(): return False
+    if required is not None or required is not "":
+        assert required.lower() in available.lower()
+        if required.lower() not in word.lower(): return False
+    if len(word) < 4: return False
 
     #Option 1 (with external function)
     return uses_only(word, available)
@@ -62,3 +63,32 @@ def check_word(word, available, required):
     #     if letter not in available.lower():
     #         return False
     # return True
+
+def word_score(word, letters):
+    """Compute the score for an acceptable word.
+
+    >>> word_score('card', 'ACDLORT')
+    1
+    >>> word_score('color', 'ACDLORT')
+    5
+    >>> word_score('cartload', 'ACDLORT')
+    15
+    """
+    if check_word(word, letters, ""):
+        if len(word) == 4: return 1
+        elif uses_all(word, letters): return len(word) + 7
+        else: return len(word)
+    return 0
+
+def uses_all_using_only(word, required):
+    """Checks whether a word uses all required letters.
+
+    >>> uses_all_using_only('banana', 'ban')
+    True
+    >>> uses_all_using_only('apple', 'api')
+    False
+    """
+    for letter in required.lower():
+        if not uses_only(word, letter):
+            return False
+    return True
