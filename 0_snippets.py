17ed397572e84cf7b0406de4118c384fd586dabb
Brent Hendricx <brenthendricx1@gmail.com>
2025-11-26 00:16:13 +0100

    Temp snippets

---
+++ 0_snippets.py
@@ -0,0 +1,20 @@
+from datetime import datetime, date, timedelta
+from time import sleep
+
+def long():
+    sleep(5)
+
+
+
+
+
+
+
+
+
+def main():
+    print("Hello, world!")
+    #HIER PROGRAMMEREN
+
+if __name__ == "__main__":
+    main()
