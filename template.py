6d6fea09bcecce087c210a0a5d80311f27266137
Brent Hendricx <brenthendricx1@gmail.com>
2025-12-01 21:13:23 +0100

    Removed unnecessary explanation. Kept bone structure.

--- template.py
+++ template.py
@@ -1,11 +1,5 @@
-def say_hello(*who: str):
-    print(f"Hello, {who}!")
+def main():
+    pass
 
-def main():                 # Standaard in elke code te zetten om "shadow names" te vermijden  (Gebruik met [if __name__ == "__main__":]    main())
-    name = "Brent"
-    say_hello(name)
-    #HIER PROGRAMMEREN
-
-if __name__ == "__main__":  # Standaard in elke code te zetten om "shadow names" te vermijden (Gebruik met [def main():])
-    GLOBAL_VARIABLE = "GLOBAL_VARIABLE"
+if __name__ == "__main__":
     main()
