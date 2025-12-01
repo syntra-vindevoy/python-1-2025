4072c3bfc5984b41d24bd0c25cadaa39f12182a2
Brent Hendricx <brenthendricx1@gmail.com>
2025-12-01 21:12:33 +0100

    Correct explanation of use //

--- hello.py
+++ hello.py
@@ -43,7 +43,7 @@ print(4 ** 2)   #int Math.Pow(4,2), Macht tot de 2de
 print(4 / 2)    #geeft altijd een float omdat het "delen" is. Geen "if" om te checken, het moet gwn snel gaan.
 print(2 / 3)    #oneindige getallen achter decimal is 16 na de komma
 print(4 // 3)   #resultaat 1    intiger deling (rest wegdoet)   6//4 is 1 met rest 2
-print(-4 // 3)  #resultaat -2
+print(-4 // 3)  #resultaat -2  Rondt af naar beneden, niet naar nul. Uitkomst is -1.33333 afgerond is dus -2
 print(7 % 2)    #resultaat 1        modulo = geeft de rest van een deling
 
 print(7 ^ 2)    #binaire rekensom (5)
