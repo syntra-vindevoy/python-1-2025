17d5af8a3b2c12d8a73f7a01ae945d40f0868687
Brent Hendricx <brenthendricx1@gmail.com>
2025-12-01 21:17:21 +0100

    Simplefied "sollution". solution does have a bug where a deleted item, gets replaced with the next, but the loop gets the next item from position. So, It skips an item.

--- tp3_ch9_ex.py
+++ tp3_ch9_ex.py
@@ -1,20 +1,19 @@
 #V1) List first names (Met Yves als eerste) naam:classroom ; y=klinker
 #V2) Remove all names starting with a vowel
 #Wondelgem, turqua, zelzate, torhaut, gegaartsbergen, lede
+from itertools import repeat
 
-classroom = ["Yves", "Brent", "Cristian", "Yves", "Femke", "Uwe"]
 
-for name in classroom:
-    if name[0] in "AEIOUY":
-#         classroom.remove(name)    #De remove loopt over de lijst, nog eens, per keer de if true is
-        delimiter = ' '
-        s = delimiter.join(classroom)
-        t = s.split(name).split(" ")
-        t = t.split(" ")
-        print(list(t))
+def wrong_way_to_take_out_things_in_list(): #Pop updates the list, with makes it so it skips the "next" item, that's been moved.
+    classroom = ["Yves", "Brent", "Cristian", "Femke", "Uwe", "Yves"]
 
-# name for
-#
-# classroom.remove(name) if name[0].lower() in "aeiouy" for name in classroom:
+    for name in classroom:
+        if name[0].upper() in "AEIOUY":
+            #         classroom.remove(name)    #De remove loopt over de lijst, nog eens, per keer de if true is
+            classroom.pop(classroom.index(name))
+            #   The pop delete deletes an entry, the list is edited, but the supposed next item is skipped since
+            #   it took the place of the deleted item. The for does +1 to the iteration.
+            #   So, since the list moves all items once to the left, and the for once to the right, you get a hop of 2.
+            print(classroom)
 
-#print(classroom)
+    print(classroom)
